08745bdfc4fa1249b314735f4aaeb51660afac61
feder8164 <sprite8164@gmail.com>
2020-02-12 21:48:09 +0100

    Interpolacja i sprawdzanie '?'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   title: string = `robaku`
 
-  dog = new Dog(`adas`,3);
-
-  
-
-  showDog(){
-    return this.dog.name
-  };
+  dog: Dog; //obiekt typu dog
 }
 
 class Dog {
   constructor(public name: string, public age: number){ 
     //musi być public żeby pole było widoczne
-    }
-       showDog(){
-         return this.name
-       }
+  }
 }
