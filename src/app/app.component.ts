ce7d1034f682777807f7f6c937397e490b8a1f17
feder8164 <sprite8164@gmail.com>
2020-02-13 18:00:53 +0100

    Two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,48 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   
-  inputText = 'Tekst';
-  maxLengt = 5;
-  colorClass = 'color';
-  logoUrl = '';
-
-  isDisable = true;
-
-  
+  profession;
+  skill;
 
   constructor(){
-    setTimeout(() => {
-      this.isDisable = false;
-    }, 3000);
-    this.change()
-    }
-
-    change(){
-      this.isHidden = true;
-      this.inputText = "Zmiana tekstu i koloru";
-      this.maxLengt = 10;
-      this.colorClass = 'color2';
-      this.logoUrl = 'https://hiro.pl/wp-content/uploads/2017/12/xd.jpg'
-    }
-
-    change1(){
-   this.colorClass = 'color';
 
   }
-  isHidden = true;
 
-  hidden(){
-    this.isHidden = false;
+  inputP(event){
+    this.profession = event.target.value;
   }
-
-  onMouseMove(event){
-    console.log('x: ' + event.clientX + ', y: ' + event.clientY);
-  }
-  onFocus(){
-    this.colorClass = 'color2'
+  inputS(event){
+    this.skill = event.target.value;
   }
 
-  onClick(event){
-    console.log(event)
-  }
 }
