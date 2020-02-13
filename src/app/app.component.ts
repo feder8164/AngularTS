656f8978701c9b95d366ba155ddf748b889c8961
feder8164 <sprite8164@gmail.com>
2020-02-13 12:06:58 +0100

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  
+  inputText = 'Tekst';
+  maxLengt = 5;
+  colorClass = 'color';
+  logoUrl = '';
+
+  isDisable = true;
+
+  
+
+  constructor(){
+    setTimeout(() => {
+      this.isDisable = false;
+    }, 3000);
+    this.change()
+    }
+
+    change(){
+      this.isHidden = true;
+      this.inputText = "Zmiana tekstu i koloru";
+      this.maxLengt = 10;
+      this.colorClass = 'color2';
+      this.logoUrl = 'https://hiro.pl/wp-content/uploads/2017/12/xd.jpg'
+    }
+
+    change1(){
+   this.colorClass = 'color';
+
+  }
+  isHidden = true;
+
+  hidden(){
+    this.isHidden = false;
+  }
 }
