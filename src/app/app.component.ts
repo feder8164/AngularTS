9e3bd9c7d73b937504905a5d538134bafcf0129e
feder8164 <sprite8164@gmail.com>
2020-02-16 18:16:02 +0100

    Odbieranie danych z komponentu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   
   tasksList = ['sprzatanie', 'gotowanie'];
+  selected(task){
+    console.log(task);
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.css"]
 })
 export class ChildComponent implements OnInit {
+  
+  @Input() 
+  tasks; 
 
-  @Input() //propty tego komponentu która jest wystawiona na zewnatrz
-  tasks; // tutaj dane zostały umieszczone
-
-  constructor() { }
-
-  ngOnInit() {
+  @Output()
+  eventTask = new EventEmitter<string>(); // emitacja danych do góry
+  
+  select(task) {
+    this.eventTask.emit(task); // wysyłam tego taska
   }
 
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  
 }
