8f83802a47a802bf2ba3890baf6b1c987ba814cc
feder8164 <sprite8164@gmail.com>
2020-02-16 09:40:50 +0100

    Lista zadań

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  
+  addTask: string;
+  taskList = new Array<work>(); //tablica obiektow
+  taskListdone = new Array<work>();
+  pushTask(){
+    this.taskList.push(new work(this.addTask));
+  }
+  removeTask(task){ // przesyłam obiekt
+    this.taskList = this.taskList.filter(e => e !== task)
+  }
+  removeTaskdone(task){ // przesyłam obiekt
+    this.taskListdone = this.taskListdone.filter(e => e !== task)
+  }
+  doneTask = task => {
+    this.taskListdone.push(task);
+    this.removeTask(task);
+  }
+  
+}
+class work{
+  constructor(public Work: string ){
+
+  }
 }
