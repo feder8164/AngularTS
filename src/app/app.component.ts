85f70ef576040e918eb3be13d1abe4ee919ee1d0
feder8164 <sprite8164@gmail.com>
2020-02-18 08:00:55 +0100

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   
   
   taskList = new Array<work>();
   taskListdone = new Array<work>();
+
+  ngOnInit(): void{
+    this.taskListdone.push(new work('Sprzatanie'), new work('Mycie'), new work('Czyszczenie'), new work('Blat'))
+  }
   
   selectedTask(task: string){
     this.taskList.push(new work(task));
--- src/app/task-list-done/task-list-done.component.ts
+++ src/app/task-list-done/task-list-done.component.ts
@@ -7,17 +7,31 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class TaskListDoneComponent implements OnInit {
 
+  //--------------@Input-------------------//Odbieranie danych
+
   @Input()
   taskListdone = [];
 
+  //--------------@Output-------------------//Wysyłanie danych
+
   @Output()
   eventRemoveTaskDone = new EventEmitter<string>();
-  
+
+
+  //--------------Metody-------------------//
 
   removeTaskdone(task){
     console.log(task)
     this.eventRemoveTaskDone.emit(task);
   }
+  //--------------ngStyle-------------------//Dynamiczne style
+
+  getColor(): string{
+    
+    return this.taskListdone.length >= 5 ? 'red' : 'green';
+     
+  }
+
 
 
   constructor() { }
