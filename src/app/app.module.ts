299e275e960fe8325a46219e6bac05847733f523
feder8164 <sprite8164@gmail.com>
2020-02-19 12:11:35 +0100

    Tworzenie_dyrektywy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AddComponent } from './add/add.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskListDoneComponent } from './task-list-done/task-list-done.component';
 import { TaskService } from './services/task.service';
+import { CheckedDirective } from './shared/checked.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AddComponent, TaskListComponent,TaskListDoneComponent],
+  declarations: [ AppComponent, HelloComponent, AddComponent, TaskListComponent,TaskListDoneComponent, CheckedDirective],
   bootstrap:    [ AppComponent ],
   providers: [TaskService]
 })
