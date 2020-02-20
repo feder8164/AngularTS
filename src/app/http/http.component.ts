3c6190450ae1edad22b988c24d406c81284a7bac
feder8164 <sprite8164@gmail.com>
2020-02-20 12:44:00 +0100

    WIDOK

---
+++ src/app/http/http.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-http',
+  templateUrl: './http.component.html',
+  styleUrls: ['./http.component.css']
+})
+export class HTTPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
