51cc09a8c495aa497dac353249f73cc471494c2f
feder8164 <sprite8164@gmail.com>
2020-02-20 09:15:21 +0100

    Przesylanie_parametrow_do_pipe

--- src/app/shared/transform.pipe.ts
+++ src/app/shared/transform.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TransformPipe implements PipeTransform {
 
-  transform(value: string, args?: any): any {
-    return value.charAt(0).toUpperCase() + value.slice(1);
+  transform(value: string, args: string = ''): any { // jesli nikt nie doda elementu dokleimy pusty string
+    return value.charAt(0).toUpperCase() + value.slice(1) + args;
   }
 
 }
